be57b4689f33cc5ad02c6bd45a482143b024ee8b
danfi <dan.filipenko@yandex.ru>
2022-02-09 05:33:43 +0200

    update player

--- resources/views/templates/player.blade.php
+++ resources/views/templates/player.blade.php
@@ -3,24 +3,23 @@
         {{ $video->title_ru }} - смотрите все серии бесплатно!
     </div>
     <div class="player-controls ">
-        @if(count($video->voices) > 1)
-            <p>В русской озвучке от:</p>
-            <div id="voices">
-                @foreach($video->voices->groupBy('voice') as $voice)
-                    <button class="player-controls-button @if($loop->first)selected @endif"
-                            onclick="voice(this)"
-                            data-voice="{{ str_slug($voice->first()->voice) }}">{{ $voice->first()->voice }}</button>
-                @endforeach
-            </div>
-        @endif
+        <p>В русской озвучке от:</p>
+        <div id="voices">
+            @foreach($video->voices->groupBy('voice') as $voice)
+                <button class="player-controls-button @if($loop->first)selected @endif"
+                        onclick="selectVoice(this)"
+                        data-voice="{{ str_slug($voice->first()->voice) }}">{{ $voice->first()->voice }}</button>
+            @endforeach
+        </div>
         <video controls type="video/mp4">
         </video>
-        <div id="series">
+        <div id="series" style="display: @if($video->type->video_type == 'Фильм')none @else block @endif">
             @foreach($video->voices->groupBy('voice') as $voice)
                 <div id="{{ str_slug($voice->first()->voice) }}" @if(!$loop->first) style="display: none" @endif>
                     @foreach($voice as $v)
-                        <button onclick="select('{{ route('stream', $v->pivot->id) }}', this)"
-                                class="player-controls-button ">Серия {{ $v->pivot->ser_number }}
+                        <button
+                            onclick="selectSeria('{{ route('stream', $v->pivot->id) }}', this, {{ $v->pivot->ser_number }})"
+                            class="player-controls-button ">Серия {{ $v->pivot->ser_number }}
                         </button>
                     @endforeach
                 </div>
@@ -32,30 +31,49 @@
 </div>
 
 <script>
-    function select(seriesId, obj)
+    function selectSeria(seriesPath, obj, serNum)
     {
-        let seriesList = document.getElementById('series');
-        currentSeries = seriesList.getElementsByClassName('selected');
+        let currentSeries = document.getElementById('series').getElementsByClassName('selected');
         if (currentSeries.item(0) !== null) {
             currentSeries[0].classList.remove('selected');
         }
 
         obj.classList.add('selected');
-        let player = document.getElementsByTagName('video');
-        player[0].setAttribute('src', seriesId);
+        let player = document.getElementsByTagName('video')[0];
+        player.setAttribute('src', seriesPath);
 
+
+        let currentVoice = document.getElementById('voices').getElementsByClassName('selected')[0];
+        localStorage.setItem('rzk-' + {{ $video->id }}, JSON.stringify({
+            voice: currentVoice.getAttribute('data-voice'),
+            seria: serNum
+        }))
     }
 
-    function voice(obj)
+    function selectVoice(obj)
     {
-        let voiceList = document.getElementById('voices');
-        let currentVoice = voiceList.getElementsByClassName('selected');
-        let beforeVoice = currentVoice[0].getAttribute('data-voice');
-        currentVoice[0].classList.remove('selected');
-        obj.classList.add('selected');
+        let currentVoice = document.getElementById('voices').getElementsByClassName('selected')[0]; //выбраная озвучка в момент нажатия
+        let beforeVoice = currentVoice.getAttribute('data-voice');//название озвучки
+        currentVoice.classList.remove('selected');//убрать выделение у  текущей озвучки
+        obj.classList.add('selected');//добавить выделение нажатой озвучке
 
-        document.getElementById(beforeVoice).style.display = 'none';
-        document.getElementById(obj.getAttribute('data-voice')).style.display = 'block';
-        document.getElementById(obj.getAttribute('data-voice')).firstElementChild.click();
+
+        document.getElementById(beforeVoice).style.display = 'none';//скрыть предыдущую озвучка
+        document.getElementById(obj.getAttribute('data-voice')).style.display = 'block';//показать серии выбраной озвучки
+        document.getElementById(obj.getAttribute('data-voice')).firstElementChild.click();//выбрать первую серию выбран. озвучки
     }
+
+    function setSeria()
+    {
+        let storage = JSON.parse(localStorage.getItem('rzk-' + {{ $video->id }}))
+
+        if (storage !== null) {
+            document.getElementById('voices').querySelector('button[data-voice="' + storage.voice + '"]').click();//выбрать озвучку
+            document.getElementById(storage.voice).getElementsByTagName('button')[storage.seria - 1].click()//выбрать серию
+        } else {
+            document.getElementById('series').firstElementChild.firstElementChild.click();
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", setSeria);
 </script>
